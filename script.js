26ccca99ba11f7091e901687ea56ba46645a3148
Lars Bendik Bjella Ramstad <brlb022025@gmail.com>
2025-02-27 11:14:43 +0100

    added and solved a small challenge on line 100

--- script.js
+++ script.js
@@ -43,3 +43,72 @@ console.log(Object.entries(user));
 //to change something in an object is easy, even though it is a constant.
 // user.age = 36;
 // user.hobbies.push("Fishing");
+
+//Below is an object array
+
+const people = [
+  {
+    name: "Tom",
+    male: true,
+    age: 32,
+    hobbies: ["Cycling", "Fusbal", "pogo"],
+  },
+  {
+    name: "Susan",
+    male: false,
+    age: 39,
+    hobbies: ["crochet", "jogging", "dog training"],
+  },
+  {
+    name: "Monica",
+    male: false,
+    age: 23,
+    hobbies: ["guitar", "bmx", "downhill"],
+  },
+  {
+    name: "Stephen",
+    male: true,
+    age: 58,
+    hobbies: ["reading", "painting", "UFC"],
+  },
+  {
+    name: "Maggie",
+    male: false,
+    age: 40,
+    hobbies: ["Sitting", "existing", "sleeping"],
+  },
+  {
+    name: "Hector",
+    male: true,
+    age: 36,
+    hobbies: ["Conquering", "court intrigue", "betrayal"],
+  },
+  {
+    name: "Philippe",
+    male: true,
+    age: 78,
+    hobbies: ["padel", "tennis", "cooking"],
+  },
+  {
+    name: "Angelica",
+    male: false,
+    age: 46,
+    hobbies: ["gossiping", "complaining", "cruelty"],
+  },
+];
+
+//Time for a challenge. add a summary to each object that says:
+// "(NAME) is an X year old man/woman who likes Y, among other things."
+// Y should be random.
+
+for (let i = 0; i < people.length; i++) {
+  //   console.log(people[i].name);
+  //   console.log(people[i].hobbies[2]);
+  const randomHobby = people[i].hobbies[Math.floor(Math.random() * 2)];
+  people[i].summary = `${people[i].name} is a ${people[i].age} year old ${
+    people[i].male ? "man" : "woman"
+  } who likes ${randomHobby} among other things.`;
+  console.log(people[i].summary);
+}
+
+console.log(people);
